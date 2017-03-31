e1c84b6f278704e263c6bd836e57f3bb89d40f32
Vladimir Smirnov <smirnowld@gmail.com>
2017-03-31 10:32:15 +0300

    * add a mechanism to recursively traverse through directory with soy templates in order to discover files buried deeper in the tree

--- src/main/java/eu/smirnowld/SoyMvcConfiguration.java
+++ src/main/java/eu/smirnowld/SoyMvcConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.FileSystemResourceLoader;
 
+import java.io.File;
 import java.io.IOException;
 
 @Configuration
@@ -14,8 +15,11 @@ public class SoyMvcConfiguration {
     @Bean
     public SoyFileSet soyFileSet() {
         try {
-            return SoyFileSet.builder().add(new FileSystemResourceLoader().getResource("classpath:static/hello.soy").getFile()).build();
-        } catch (IOException e) {
+            final File soyTemplatesLocation = new FileSystemResourceLoader().getResource("classpath:/static/soy/").getFile();
+            final SoyFileSet.Builder soyFileSetBuilder = SoyFileSet.builder();
+            populateBuilderWithSoyTemplates(soyFileSetBuilder, soyTemplatesLocation);
+            return soyFileSetBuilder.build();
+        } catch (final IOException e) {
             e.printStackTrace();
             throw new RuntimeException(e);
         }
@@ -29,5 +33,25 @@ public class SoyMvcConfiguration {
     @Bean
     public SoyViewResolver soyViewResolver() {
         return new SoyViewResolver(soyTofu());
+    }
+
+    private void populateBuilderWithSoyTemplates(final SoyFileSet.Builder soyFiles,
+                                                 final File directory) {
+        final File[] files = directory.listFiles();
+        if (files != null) {
+            for (final File file : files) {
+                if (file.isFile()) {
+                    if (file.getName().endsWith(".soy")) {
+                        soyFiles.add(file);
+                    }
+                } else if (file.isDirectory()) {
+                    populateBuilderWithSoyTemplates(soyFiles, file);
+                }
+            }
+        } else {
+            throw new IllegalArgumentException(
+                    "Failed to populate SoyFileSet.Builder with soy templates from directory [" + directory + "]. Check that it is" + " indeed a directory " +
+                            "and not empty!");
+        }
     }
 }
